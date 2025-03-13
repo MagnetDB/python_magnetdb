41d0240d3e9936be9515e31c8f2288160d234c1e
Rémi Caumette <remicaumette@icloud.com>
2025-03-13 13:52:42 +0100

    chore(web): remove client overlay configuration from devServer

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,11 +1,5 @@
 module.exports = {
   devServer: {
-    client: {
-      overlay: {
-        warnings: false,
-        errors: false
-      }
-    },
     allowedHosts: [
       'lncmig.local',
       'magnetdb-dev.local',
