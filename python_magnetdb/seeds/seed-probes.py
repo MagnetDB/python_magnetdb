d8aba162421b6d66308052d1d48c42c8d4fcf723
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2026-02-04 12:48:39 +0100

    fix typo

--- python_magnetdb/seeds/seed-probes.py
+++ python_magnetdb/seeds/seed-probes.py
@@ -1,23 +1,26 @@
 """
 Create probes
 """
+
 from os import getenv
 
-from .crud import create_probe, query_magnet, query_probes
+from .crud import create_probe, query_magnet, query_probe
 from ..models.probe import ProbeType
 
 # Get parts from previous defs
-HLtest = query_magnet('HL-test')
-Vprobes = query_probes('HLtest-probes')
+HLtest = query_magnet("HL-test")
+Vprobes = query_probe("HLtest-probes")
 if Vprobes is not None:
     print("HLtest-probes already exists, skipping creation")
 else:
     print("Creating HLtest-probes")
-    Vprobes = create_probe({
-        'name':'HLtest-probes',
-        'type': ProbeType.VOLTAGE,
-        'labels': ['U1', 'U2', 'U3'],
-        'points': [[0,0,0], [0,0,1], [0,0,2]],
-        'description': 'voltage probe per Helix with fakes coordinates',
-        'magnet': HLtest
-    })
+    Vprobes = create_probe(
+        {
+            "name": "HLtest-probes",
+            "type": ProbeType.VOLTAGE,
+            "labels": ["U1", "U2", "U3"],
+            "points": [[0, 0, 0], [0, 0, 1], [0, 0, 2]],
+            "description": "voltage probe per Helix with fakes coordinates",
+            "magnet": HLtest,
+        }
+    )
