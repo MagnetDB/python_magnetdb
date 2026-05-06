5b8f8a06dca90cc9e8dc2118e50d677ec01f5355
Christophe Trophime <christophe.trophime@lncmi.cnrs.fr>
2026-05-06 13:40:31 +0200

    update to support node20

--- web/vue.config.js
+++ web/vue.config.js
@@ -3,6 +3,17 @@ module.exports = {
     allowedHosts: [
       'lncmig.local',
       'magnetdb-dev.local',
-    ]
-  }
+    ],
+    client: {
+      webSocketURL: 'auto://0.0.0.0:0/ws',
+    },
+  },
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        stream: false,
+        assert: false,
+      },
+    },
+  },
 }
